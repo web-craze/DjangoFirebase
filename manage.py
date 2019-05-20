c6f63c5c70e53daa6fa06d51706f9f2a1957caf4
KurtLassen <Kurt.Lassen@outlook.com>
2019-05-20 09:49:16 +0800

    manage file edited

--- manage.py
+++ manage.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 # manage.py : main start point
+# Firstly, routing manage.py
 import os
 import sys
+
 
 if __name__ == '__main__':
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoFirebaseProject.settings')
